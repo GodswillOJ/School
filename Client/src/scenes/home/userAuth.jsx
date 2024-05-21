397dbdfb0b9a862566bc5472c2a004381c6ae240
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-05-21 21:12:37 +0100

    kknk

--- Client/src/scenes/home/userAuth.jsx
+++ Client/src/scenes/home/userAuth.jsx
@@ -137,7 +137,7 @@ export const Login = () => {
       localStorage.setItem('access_token', response.data.access_token);
       localStorage.setItem('userID', response.data.userID); // Ensure userID is stored here
   
-      navigate('/');
+      navigate('/dashboard');
       window.location.reload(); // Reload the page after navigation
     } catch (error) {
       console.error('Error in verifying user:', error.message);
