54e3b0bc89e381ba27a1ae57e724a1b6f336a4f5
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-05-26 16:05:35 +0100

    jkk

--- Backend/src/controllers/general.js
+++ Backend/src/controllers/general.js
@@ -26,7 +26,7 @@ export const insertUser = async (req, res) => {
 
   // Hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
-  const image = req.file.filename
+  const image = req.file.path
 
   const newUser = new User({ username, email, image: image, password: hashedPassword, role: 'user' });
 
--- Backend/src/routes/general.js
+++ Backend/src/routes/general.js
@@ -20,12 +20,16 @@ router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
 // Serve static files from the "client/src/assets/images" directory
-router.use('/assets/', express.static(path.join(__dirname, '../../../Client/src/assets/')));
+// router.use('/public', express.static(path.join(__dirname, '../public')));
+router.use('/public', express.static('public'));
 
 // Setting up multer for image uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../../../Client/src/assets/'));
+    // const uploadPath = path.join(__dirname, '../public');
+    // console.log(`Uploading file to: ${uploadPath}`);
+    // cb(null, uploadPath);
+    cb(null, 'public');
   },
   filename: function (req, file, cb) {
     const filename = Date.now() + path.extname(file.originalname);
