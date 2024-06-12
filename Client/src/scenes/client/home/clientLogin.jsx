5e1d5ee46b69d20b0836e6105541319660dd3a20
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-06-12 22:13:17 +0100

    kknk

--- Client/src/scenes/client/home/clientLogin.jsx
+++ Client/src/scenes/client/home/clientLogin.jsx
@@ -1,7 +1,7 @@
 // VerifyLogin.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faInstagram, faTwitter, faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
@@ -198,14 +198,58 @@ export const LoginClient = () => {
   );
 };
 
-export const VerifyClient = () => {
+ export const VerifyClient = ({ isLoggedIn }) => {
+  const { token, id } = useParams(); // Extract the token and ID parameters from the URL
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
+
+  console.log('Token:', token);
+  console.log('ID:', id);
+
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (isLoggedIn) {
+          const accessToken = localStorage.getItem('access_token');
+          const response = await axios.get(`https://gotech-ecommerce.onrender.com/api/userVerifyMail/${id}`, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          });
+          console.log('User Data:', response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+
+  }, [isLoggedIn, id]);
+
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <>
-      <div className="userVerifyMail">
-        <h3>
-          Your mail has been verified. Click <Link to="/loginUser">Login</Link> to get redirected to login page
-        </h3>
-      </div>
-    </>
-  )
+    <div className="VerifyCont">
+        <div>
+            <h1>Admin Verify Mail</h1>
+            <div>
+                <p>
+                    Email verified please click to redirect to login<Link to='/loginUser'> proceed to login</Link>
+                </p>
+            </div>
+        </div>
+    </div>
+  );
 };
